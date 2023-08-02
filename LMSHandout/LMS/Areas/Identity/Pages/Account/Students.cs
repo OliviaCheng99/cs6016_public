23cb48b8579eefb6c507cff43fa6137b2907788c
Olivia <olivia.chengman@gmail.com>
2023-08-02 13:38:08 -0600

    backup

---
+++ LMSHandout/LMS/Areas/Identity/Pages/Account/Students.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using LMS.Models.LMSModels;
+
+namespace LMS.Areas.Identity.Pages.Account
+{
+    internal class Students : Student
+    {
+        public string UId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ LMSHandout/LMS/Controllers/StudentController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LMS.Models.LMSModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LMS.Controllers
+{
+    [Authorize(Roles = "Student")]
+    public class StudentController : Controller
+    {
+        private LMSContext db;
+        public StudentController(LMSContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+
+        public IActionResult Class(string subject, string num, string season, string year)
+        {
+            ViewData["subject"] = subject;
+            ViewData["num"] = num;
+            ViewData["season"] = season;
+            ViewData["year"] = year;
+            return View();
+        }
+
+        public IActionResult Assignment(string subject, string num, string season, string year, string cat, string aname)
+        {
+            ViewData["subject"] = subject;
+            ViewData["num"] = num;
+            ViewData["season"] = season;
+            ViewData["year"] = year;
+            ViewData["cat"] = cat;
+            ViewData["aname"] = aname;
+            return View();
+        }
+
+
+        public IActionResult ClassListings(string subject, string num)
+        {
+            System.Diagnostics.Debug.WriteLine(subject + num);
+            ViewData["subject"] = subject;
+            ViewData["num"] = num;
+            return View();
+        }
+
+
+        /*******Begin code to modify********/
+
+        /// <summary>
+        /// Returns a JSON array of the classes the given student is enrolled in.
+        /// Each object in the array should have the following fields:
+        /// "subject" - The subject abbreviation of the class (such as "CS")
+        /// "number" - The course number (such as 5530)
+        /// "name" - The course name
+        /// "season" - The season part of the semester
+        /// "year" - The year part of the semester
+        /// "grade" - The grade earned in the class, or "--" if one hasn't been assigned
+        /// </summary>
+        /// <param name="uid">The uid of the student</param>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetMyClasses(string uid)
+        {
+            var classes = (from e in db.Enrolleds
+                           join c in db.Classes on e.Class equals c.ClassId
+                           join co in db.Courses on c.Listing equals co.CatalogId
+                           where e.Student == uid
+                           select new
+                           {
+                               subject = co.Department,
+                               number = co.Number,
+                               name = co.Name,
+                               season = c.Season,
+                               year = c.Year,
+                               grade = e.Grade ?? "--"
+                           }).ToArray();
+
+            return Json(classes);
+        }
+
+        /// <summary>
+        /// Returns a JSON array of all the assignments in the given class that the given student is enrolled in.
+        /// Each object in the array should have the following fields:
+        /// "aname" - The assignment name
+        /// "cname" - The category name that the assignment belongs to
+        /// "due" - The due Date/Time
+        /// "score" - The score earned by the student, or null if the student has not submitted to this assignment.
+        /// </summary>
+        /// <param name="subject">The course subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
+        /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
+        /// <param name="uid"></param>
+        /// <returns>The JSON array</returns>
+        public IActionResult GetAssignmentsInClass(string subject, int num, string season, int year, string uid)
+        {
+            // Step 1: Get CatalogID using subject and num
+            var catalogId = db.Courses
+                              .Where(c => c.Department == subject && c.Number == num)
+                              .Select(c => c.CatalogId)
+                              .FirstOrDefault();
+
+            if (catalogId == 0)
+            {
+                return Json(new { error = "Course not found" });
+            }
+
+            // Step 2: Find a Class using CatalogID
+            var classId = db.Classes
+                            .Where(c => c.Listing == catalogId && c.Season == season && c.Year == year)
+                            .Select(c => c.ClassId)
+                            .FirstOrDefault();
+
+            if (classId == 0)
+            {
+                return Json(new { error = "Class not found" });
+            }
+
+            //
+
+            return Json(assignments);
+        }
+
+
+
+        /// <summary>
+        /// Adds a submission to the given assignment for the given student
+        /// The submission should use the current time as its DateTime
+        /// You can get the current time with DateTime.Now
+        /// The score of the submission should start as 0 until a Professor grades it
+        /// If a Student submits to an assignment again, it should replace the submission contents
+        /// and the submission time (the score should remain the same).
+        /// </summary>
+        /// <param name="subject">The course subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester for the class the assignment belongs to</param>
+        /// <param name="year">The year part of the semester for the class the assignment belongs to</param>
+        /// <param name="category">The name of the assignment category in the class</param>
+        /// <param name="asgname">The new assignment name</param>
+        /// <param name="uid">The student submitting the assignment</param>
+        /// <param name="contents">The text contents of the student's submission</param>
+        /// <returns>A JSON object containing {success = true/false}</returns>
+        public IActionResult SubmitAssignmentText(string subject, int num, string season, int year,
+          string category, string asgname, string uid, string contents)
+        {           
+            return Json(new { success = false });
+        }
+
+
+        /// <summary>
+        /// Enrolls a student in a class.
+        /// </summary>
+        /// <param name="subject">The department subject abbreviation</param>
+        /// <param name="num">The course number</param>
+        /// <param name="season">The season part of the semester</param>
+        /// <param name="year">The year part of the semester</param>
+        /// <param name="uid">The uid of the student</param>
+        /// <returns>A JSON object containing {success = {true/false}. 
+        /// false if the student is already enrolled in the class, true otherwise.</returns>
+        public IActionResult Enroll(string subject, int num, string season, int year, string uid)
+        {          
+            return Json(new { success = false});
+        }
+
+
+
+        /// <summary>
+        /// Calculates a student's GPA
+        /// A student's GPA is determined by the grade-point representation of the average grade in all their classes.
+        /// Assume all classes are 4 credit hours.
+        /// If a student does not have a grade in a class ("--"), that class is not counted in the average.
+        /// If a student is not enrolled in any classes, they have a GPA of 0.0.
+        /// Otherwise, the point-value of a letter grade is determined by the table on this page:
+        /// https://advising.utah.edu/academic-standards/gpa-calculator-new.php
+        /// </summary>
+        /// <param name="uid">The uid of the student</param>
+        /// <returns>A JSON object containing a single field called "gpa" with the number value</returns>
+        public IActionResult GetGPA(string uid)
+        {            
+            return Json(null);
+        }
+                
+        /*******End code to modify********/
+
+    }
+}
+
