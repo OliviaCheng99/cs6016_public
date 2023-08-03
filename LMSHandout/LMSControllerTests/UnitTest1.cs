2cde9a5281d6aa963996176565b95f299bf80660
Olivia <olivia.chengman@gmail.com>
2023-08-02 20:07:34 -0600

    add test

--- LMSHandout/LMSControllerTests/UnitTest1.cs
+++ LMSHandout/LMSControllerTests/UnitTest1.cs
@@ -13,7 +13,6 @@ namespace LMSControllerTests
     {
         // Uncomment the methods below after scaffolding
         // (they won't compile until then)
-
         [Fact]
         public void Test1()
         {
@@ -24,11 +23,17 @@ namespace LMSControllerTests
             Assert.NotNull(allDepts);
             Assert.NotNull(allDepts.Value);
 
-            dynamic x = allDepts.Value;
+            // Convert to a collection of anonymous types
+            var departments = (allDepts.Value as IEnumerable<object>)?.ToList();
+            Assert.NotNull(departments);
+            Assert.Single(departments);
 
-            Assert.Equal(1, x.Length);
-            Assert.Equal("CS", x[0].subject);
+            // Use reflection to access properties
+            var firstDepartment = departments[0];
+            var subject = firstDepartment.GetType().GetProperty("subject")?.GetValue(firstDepartment);
+            Assert.Equal("CS", subject);
         }
+
 
 
         ///// <summary>
@@ -56,19 +61,19 @@ namespace LMSControllerTests
             db.Courses.Add(new Course { CatalogId = 1, Name = "Introduction to Computer Science", Number = 5530, Department = "CS" });
 
             // Adding a professor
-            db.Professors.Add(new Professor { UId = "u1234567", FName = "John", LName = "Doe" });
+            db.Professors.Add(new Professor { UId = "u1234567", FName = "John", LName = "Doe", WorksIn = "CS"});
 
             // Adding a class
-            db.Classes.Add(new Class { ClassId = 1, Season = "Fall", Year = 2023, Listing = 1, TaughtBy = "u1234567" });
+            db.Classes.Add(new Class { ClassId = 1, Season = "Fall", Year = 2023, Listing = 1, TaughtBy = "u1234567", Location = "TEST" });
 
             // Adding a student
-            db.Students.Add(new Student { UId = "u7654321", FName = "Jane", LName = "Smith" });
+            db.Students.Add(new Student { UId = "u7654321", FName = "Jane", LName = "Smith", Major = "CS" });
 
             // Adding an assignment category
             db.AssignmentCategories.Add(new AssignmentCategory { CategoryId = 1, InClass = 1, Name = "Homework", Weight = 50 });
 
             // Adding an assignment
-            db.Assignments.Add(new Assignment { AssignmentId = 1, Category = 1, Name = "HW1", MaxPoints = 100, Due = DateTime.Now });
+            db.Assignments.Add(new Assignment { AssignmentId = 1, Category = 1, Name = "HW1", MaxPoints = 100, Due = DateTime.Now, Contents = "TESTING" });
 
             db.SaveChanges();
 
@@ -77,7 +82,11 @@ namespace LMSControllerTests
 
         private IServiceProvider? NewServiceProvider()
         {
-            throw new NotImplementedException();
+            var serviceProvider = new ServiceCollection()
+                .AddDbContext<LMSContext>()
+                .BuildServiceProvider();
+
+            return serviceProvider;
         }
     }
 }
